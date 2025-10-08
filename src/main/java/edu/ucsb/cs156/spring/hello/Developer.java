51cf6f11f420c58c656e746ec75f56bfc7718b83
MaxC <mchang900@gmail.com>
2025-10-07 17:40:19 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-f25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Max";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "max-changg";
     }
 
     /**
@@ -40,13 +40,12 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-04");
+        team.addMember("Ajay");
+        team.addMember("Anthony");
+        team.addMember("Chiran");
+        team.addMember("Dave");
+        team.addMember("Ryan");
         return team;
     }
 }
